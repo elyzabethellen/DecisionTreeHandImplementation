ad1e9ee86e918e85652e74c95eddca17e105605d
robomancer <elizabeth@therobomancer.com>
2017-09-20 09:46:47 -0600

    order!

--- treeTest.R
+++ treeTest.R
@@ -10,20 +10,33 @@ source("util.R")
 args <- commandArgs(trailingOnly = TRUE)
 #read training data
 training <- read_csv(args[1])
-#add dummy column to mimic raw data before feature extraction from book data
+#add dummy column to mimic raw data before features from book data
+#for ANY data, ensure that classification result is in the last column!
 if (args[1] == "weatherTraining.csv"){
   add_column(training, a = 0, .before = "Outlook" )
 }
 
 #----------------------------------------------------------------#
-# feature extraction
+# feature extraction and pre-processing
 # call Danny's feature extraction methods and append as columns
-# create a vector of what the info gain is for each feature and pair with 
-###the name of that feature
+# with values for what the info gain is for each data point for
+# each feature 
+
+#---i.e., for our features
+# call first letter
+# call last letter
+# call frequencyOfAppearance
+# call permutations
+
+#--for the book features the fields are already populated
 
 
+#now get the info gain for each feature and store as a tuple, sort by the highest gain.
+#Gain(S, Outlook) = 0.246
+#Gain(S, Temperature) = 0.029
+#Gain(S, Humidity) = 0.151
+#Gain(S, Wind) = 0.048 
 
-#--------deal with NAs and NANs here in more complex datasets----#
 
 #calculate entropy of data set
 #Entropy(S) = 0.940286
@@ -33,16 +46,6 @@ outcomes <- unlist(strsplit(args[3], ","))
 dEnt <- datasetEntropy(training$Play, outcomes)
 cat("Dataset Entropy = ",dEnt)
 
-#---------get args for attribute vector? Probably-------------------------#
-#calculate info gain for all attributes at the outset.
 
-
-#Gain(S, Outlook) = 0.246
-
-#Gain(S, Temperature) = 0.029
-
-#Gain(S, Humidity) = 0.151
-
-#Gain(S, Wind) = 0.048 
 
 #the highest gain is the root of the tree
