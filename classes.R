afada3473d7ca1a005f09f8e7472a9a42fddeba8
robomancer <elizabeth@therobomancer.com>
2017-09-23 17:48:36 -0600

    class update, r object fun

--- classes.R
+++ classes.R
@@ -1,5 +1,43 @@
 #class file
 
-setClass("root", slots = list(children = "list"))
-setClass("node", slots = list(children = "list", parent = "list"))
-setClass("leaf", slots = list(parent = "list"))
+ID3Tree <- setClass(
+  "id3tree",
+  slots = c(
+    children = "list"
+  ),
+  prototype = list(
+    children = c()
+  )
+)
+
+Root <- setClass(
+  "root", 
+  slots = c(
+    children = "list"
+    ),
+  prototype = list(
+    children = c()
+  )
+)
+
+#setting parent to list type for easiness even though it's a single object, it could be a root or a node
+Node <- setClass(
+  "node", 
+  slots = c(
+    children = "list", 
+    parent = "list"),
+  prototype = list(
+    children = c(),
+    parent = c()
+  )
+  )
+
+Leaf <- setClass(
+  "leaf", slots = 
+    c(
+      parent = "list"
+      ),
+  prototype = list(
+    parent = c()
+  )
+  )
